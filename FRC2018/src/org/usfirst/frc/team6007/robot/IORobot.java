da8504c2896cda239b0af543b2bc8a434f542375
Ebony Nicholas <35989154+EbonyN@users.noreply.github.com>
2018-02-10 12:53:58 +1100

    Update IORobot.java

--- FRC2018/src/org/usfirst/frc/team6007/robot/IORobot.java
+++ FRC2018/src/org/usfirst/frc/team6007/robot/IORobot.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team6007.robot;
 
 import com.kauailabs.navx.frc.AHRS;
+//import com.kauailabs.navx.frc.AHRS.SerialDataType;
 
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -26,3 +27,18 @@ public IORobot(){
      DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
   }
 }
+
+/* things to use when measuring and adjusting vears/turns to be put in robot ?
+ yaw is left and right of an object on an axis running up and down
+ goes in operator control part:
+  
+  SmartDashboard.putNumber(   "IMU_Yaw",              ahrs.getYaw());
+  SmartDashboard.putNumber(   "IMU_TotalYaw",         ahrs.getAngle());
+  SmartDashboard.putNumber(   "IMU_YawRateDPS",       ahrs.getRate());
+ 
+  SmartDashboard.putNumber(   "YawAxis",              yaw_axis.board_axis.getValue() );
+  SmartDashboard.putString(   "YawAxisDirection",     yaw_axis.up ? "Up" : "Down" );
+  
+or just compass:      SmartDashboard.putNumber(   "IMU_CompassHeading",   ahrs.getCompassHeading());
+if compassHeading when driving forward/backward changes by 5? or more degrees, reverse direction by change. Figure if left or right
+*/
