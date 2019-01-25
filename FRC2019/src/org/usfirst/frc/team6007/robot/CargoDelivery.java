486be67257bbeaff34caa7d3b2f9fda6e33d3dc3
Ebony Nicholas <35989154+EbonyN@users.noreply.github.com>
2019-01-25 12:47:01 +1100

    Update CargoDelivery.java
    added comments

--- FRC2019/src/org/usfirst/frc/team6007/robot/CargoDelivery.java
+++ FRC2019/src/org/usfirst/frc/team6007/robot/CargoDelivery.java
@@ -1,6 +1,6 @@
 /***********************************************************
-* 
-* 
+* 6007 2019 cargo (ball) delivery/arm movement
+* Editors Hayley, Ebony, Mr Legge
 ************************************************************/
 
 package org.usfirst.frc.team6007.robot;
@@ -18,13 +18,13 @@ public class CargoDelivery{
 		}
 	}
 	
-  	public void cargoLoad(){
+  	public void cargoLoad(){                               //moves arm to range of position
 		while(robotIO.ballPotentiometer.get >= RobotMap.HATCH_DELIVERY_LOWER_lIMIT && robotIO.ballPotentiometer.get <= RobotMap.HATCH_DELIVERY_UPPER_lIMIT){
 			//run motor to move to positon
 		}
 	}
 	
-	public void loaderToHome(){
+	public void loaderToHome(){                           //moves arm back to in/starting position
 		while(robotIO.homeCargoSwitch.get()){
 			//run motor to move to positon
 		}
